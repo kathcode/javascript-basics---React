1b8f889356cc3019d760cbcf73107779498fcff9
Lesly Catalina Meneses Carmona <lesly.meneses@globant.com>
2019-12-02 10:11:23 -0500

    Edit to receive the topic

--- src/components/Code/index.js
+++ src/components/Code/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import { topics } from '../topics';
 
 const Code = ({ selectedTopic }) => {
-
-  const getCodeSnipped = () => {
-    const snipped = topics[selectedTopic].component;
-
-    return `${snipped}`;
-  }
+  const Topic = topics[selectedTopic].component;
 
   return (
     <React.Fragment>
-      <header>Code</header>
-      <SyntaxHighlighter lineNumberStyle language="javascript" style={dracula}>
-        {getCodeSnipped()}
-      </SyntaxHighlighter>
+      <header>Definition</header>
+      <Topic />
     </React.Fragment>
   );
 }
